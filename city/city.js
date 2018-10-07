2963dcbdcfff9a0b75371c94523ef38a2b1b83d3
Gleb Devyatkin <sovgvd@gmail.com>
2018-10-07 20:38:56 +1300

    Decorations

--- city/city.js
+++ city/city.js
@@ -5,31 +5,55 @@ var city = function (c) {
 	this.gen = false;
 	this.blocks = [];
 	this.id = false;
-	this.count_buildings = {};
+	this.count_objects = { };
 	
 	this.init = function () {
 		this.m = []; this.w = [];
+
+		this.c.buildings.min=[false, false, false];
+		this.c.buildings.max=[false, false, false];
+		for (var i = 0; i < this.c.buildings.types.length; i++ ) {
+			if (this.c.buildings.min[0] === false || this.c.buildings.types[i].size[0] < this.c.buildings.min[0]) this.c.buildings.min[0] = this.c.buildings.types[i].size[0];
+			if (this.c.buildings.min[1] === false || this.c.buildings.types[i].size[1] < this.c.buildings.min[1]) this.c.buildings.min[1] = this.c.buildings.types[i].size[1];
+			if (this.c.buildings.max[0] === false || this.c.buildings.types[i].size[0] > this.c.buildings.max[0]) this.c.buildings.max[0] = this.c.buildings.types[i].size[0];
+			if (this.c.buildings.max[1] === false || this.c.buildings.types[i].size[1] > this.c.buildings.max[1]) this.c.buildings.max[1] = this.c.buildings.types[i].size[1];
+		}
+
+		for (var i = 0; i < this.c.decorations.types.length; i++ ) {
+			if (this.c.buildings.min[0] === false || this.c.decorations.types[i].size[0] < this.c.buildings.min[0]) this.c.buildings.min[0] = this.c.decorations.types[i].size[0];
+			if (this.c.buildings.min[1] === false || this.c.decorations.types[i].size[1] < this.c.buildings.min[1]) this.c.buildings.min[1] = this.c.decorations.types[i].size[1];
+			if (this.c.buildings.max[0] === false || this.c.decorations.types[i].size[0] > this.c.buildings.max[0]) this.c.buildings.max[0] = this.c.decorations.types[i].size[0];
+			if (this.c.buildings.max[1] === false || this.c.decorations.types[i].size[1] > this.c.buildings.max[1]) this.c.buildings.max[1] = this.c.decorations.types[i].size[1];
+		}
+
+		// expend maximal object boundaries to fit including space_to_road for buildings
+		this.c.buildings.min[0]=this.c.buildings.min[0]+this.c.buildings.space_to_road*2;
+		this.c.buildings.min[1]=this.c.buildings.min[1]+this.c.buildings.space_to_road*2;
+		this.c.buildings.max[0]=this.c.buildings.max[0]+this.c.buildings.space_to_road*2;
+		this.c.buildings.max[1]=this.c.buildings.max[1]+this.c.buildings.space_to_road*2;
+
 		for (var y=0;y<this.c.size[1];y++) {
 			this.m[y] = []; this.w[y] = [];
 			for (var x=0;x<this.c.size[0];x++) {
-				if (x==0 || y==0 || x==this.c.size[0]-1 || y==this.c.size[1]-1) {
-					this.m[y][x] = { type: "border", id: 1 };
-					this.w[y][x] = 0;
-				} else {
+				//if (x==0 || y==0 || x==this.c.size[0]-1 || y==this.c.size[1]-1) {
+				//	this.m[y][x] = { type: "border", id: 1 };
+				//	this.w[y][x] = 0;
+				//} else {
 					this.m[y][x] = { type: "none", id: 0 };
-					this.w[y][x] = 0;
-				}
+					this.w[y][x] = 1;
+				//}
 			}
 		}
 		if (this.c.f.type == 'lsystem') {
-			this.gen_road_lsystem(true, [1 ,1 , parseInt(this.c.size[0])-2, parseInt(this.c.size[1])-2, 2]);
+			this.gen_road_lsystem(Math.random()>0.5?true:false, [0 ,0 , parseInt(this.c.size[0])-1, parseInt(this.c.size[1])-1, 2]);
 		}
 		this.id = this.gen_buildings_blocks_clear();
-		this.gen_buildings_blocks();		
+		this.gen_buildings_blocks();	
+		//this.gen_finish();	
 	}
 	
 	this.get = function () {
-		return this.m;
+		return {map: this.m, walk: this.w };
 	}
 	
 	// L-system
@@ -154,13 +178,8 @@ var city = function (c) {
 				if (max_id < this.blocks[i][4]) max_id=this.blocks[i][4];
 			}
 		}
-		for (var i = tmp.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var temp = tmp[i];
-			tmp[i] = tmp[j];
-			tmp[j] = temp;
-		}
-		this.blocks = tmp;
+
+		this.blocks = this.shuffle(tmp);
 
 		for (var i = 0; i < this.blocks.length; i++) {
 			b = this.blocks[i];
@@ -194,51 +213,125 @@ var city = function (c) {
 		}
 		return max_id;
 	}
-	
-	this._gen_buildings_blocks_check_collision = function (pos, building, fit) {
+
+
+	this._gen_check_onmap = function (pos) {
+		if (pos[0] >= 0 && pos[0] < this.c.size[0] && pos[1] >= 0 && pos[1] < this.c.size[1]) {
+			return true;
+		}
+		return false;
+	}
+
+	this._gen_check_collission = function (type, pos, obj) {
 		var fitted = true;
-		var delta = this.c.buildings.space_between_buildings;
-		//console.log("check", pos[0], pos[1], building);
-		if (pos[0] >= 0 && pos[1] >= 0 && pos[0]+building.size[0] <= this.c.size[0] && pos[1]+building.size[1] <= this.c.size[1]) {
-			for (var y = pos[1]; y < pos[1]+building.size[1]; y++) {
-				for (var x = pos[0]; x < pos[0]+building.size[0]; x++) {
-					if (this.m[y][x].id > 0) {
-						fitted = false;
-						break;
-					}
+		for (var y = pos[1]; y < pos[1]+obj.size[1]; y++) {
+			for (var x = pos[0]; x < pos[0]+obj.size[0]; x++) {
+				if (this.m[y][x].id > 0 && !(obj.merge && typeof this.m[y][x].sub_type != 'undefined' && this.m[y][x].type == type && this.m[y][x].sub_type == obj.type)) {
+					fitted = false;
+					break;
 				}
-				if (!fitted) break;
 			}
-			// space between buildings
-			if (fitted === true && delta > 0) {
-				for (var y = pos[1]-delta; y < pos[1]+building.size[1]+delta; y++) {
-					if ((pos[0]-delta >= 0 && this.m[y][pos[0]-delta].type  == 'building') || (pos[0]+building.size[0]+delta-1 < this.c.size[0] && this.m[y][pos[0]+building.size[0]+delta-1].type  == 'building')) {
+			if (!fitted) break;
+		}
+		return fitted;
+	}
+	
+	this._gen_check_border = function (type, pos, obj, delta, delta_type) {
+		var fitted = true;
+		for (var y = pos[1]-delta; y < pos[1]+obj.size[1]+delta; y++) {
+			for (var x = pos[0]-delta; x < pos[0]+obj.size[0]+delta; x++) {
+				try {
+					if (
+						this._gen_check_onmap([x,y]) && 
+						!(obj.merge && typeof this.m[y][x].sub_type != 'undefined' && this.m[y][x].type == type && this.m[y][x].sub_type == obj.type) && 
+						this.m[y][x].type != 'border' && 
+						(this.m[y][x].type == delta_type || ('any' == delta_type && this.m[y][x].type != "none"))
+					) {
 						fitted = false;
 						break;
 					}
+				} catch (e) {
+					console.warn("Border", e, x, y, this._gen_check_onmap([x,y]));
 				}
-				for (var x = pos[0]-delta; x < pos[0]+building.size[0]+delta; x++) {
-					if ((pos[1]-delta >= 0 && this.m[pos[1]-delta][x].type  == 'building') || (pos[1]+building.size[1]+delta-1 < this.c.size[1] && this.m[pos[1]+building.size[1]+delta-1][x].type  == 'building')) {
-						fitted = false;
-						break;
+			}
+			if (!fitted) break;
+		}
+		return fitted;
+	}
+
+	this._gen_check_distance = function (pos, obj) {
+		var distance_to_center = [
+			Math.abs((pos[0]-obj.size[0]/2)/(this.c.size[0]/2)-1)*100,
+			Math.abs((pos[1]-obj.size[1]/2)/(this.c.size[1]/2)-1)*100
+			];
+		if (obj.distance === false || (
+				 (distance_to_center[0] <= obj.distance[1] && distance_to_center[1] <= obj.distance[1]) &&
+				!(distance_to_center[0] <= obj.distance[0] && distance_to_center[1] <= obj.distance[0])
+			)) {
+			return true;
+		}
+		return false;
+	}
+
+	this._gen_update_limits = function () {
+		for (var b = 0; b < this.c.buildings.types.length; b++) {
+			this.count_objects["per_block_buildings."+this.c.buildings.types[b].type] = 0;
+		}
+		for (var b = 0; b < this.c.decorations.types.length; b++) {
+			this.count_objects["per_block_decorations."+this.c.decorations.types[b].type] = 0;
+		}
+	}
+	
+	this._gen_check_limit = function (type, obj) {
+		if (typeof this.count_objects[type+"."+obj.type] == 'undefined') this.count_objects[type+"."+obj.type] = 0;
+		this.count_objects[type+"."+obj.type]++;
+		this.count_objects["per_block_"+type+"."+obj.type]++;
+		if (this.c.limits.types[type+"."+obj.type].max > -1 && this.count_objects[type+"."+obj.type] > this.c.limits.types[type+"."+obj.type].max) return false;
+		if (this.c.limits.types[type+"."+obj.type].max_per_block > -1 && this.count_objects["per_block_"+type+"."+obj.type] > this.c.limits.types[type+"."+obj.type].max_per_block) return false;
+		return true;
+	}
+	
+	this._gen_set_object = function (type, pos, obj) {
+		this.id++;
+		for (var y = pos[1]; y < pos[1]+obj.size[1]; y++) {
+			for (var x = pos[0]; x < pos[0]+obj.size[0]; x++) {
+				this.m[y][x].id = this.id;
+				this.m[y][x].type = type;
+				this.m[y][x].sub_type = obj.type;
+				this.w[y][x] = 0;	// TODO chech walkable map
+			}
+		}
+	}
+	
+	this._gen_buildings_blocks_check_collision = function (pos, building, fit, position) {
+		var fitted = true;
+		var delta = 0;
+		var deltas = { 
+			"any": this.c.buildings.space_between_items,
+			"road" : this.c.buildings.space_to_road,
+		}
+
+		if (
+			pos[0] >= 0 && pos[1] >= 0 && 
+			pos[0]+building.size[0] <= this.c.size[0] && pos[1]+building.size[1] <= this.c.size[1] &&
+			(building.position.indexOf('contour')>-1 || building.position.indexOf(position)>-1) &&
+			this._gen_check_distance(pos, building) &&  
+			fitted
+		) {
+			fitted = this._gen_check_collission("buildings", pos, { size: [building.size[0], building.size[1]] });
+			// space between buildings
+			for (var delta_type in deltas) {
+				if (deltas[delta_type] != 'undefined') {
+					delta = deltas[delta_type];
+					if (fitted === true && delta > 0) {
+						fitted = this._gen_check_border ("buildings", pos, building, delta, delta_type);
 					}
 				}
 			}
 			if (fitted === true && fit === true) {
-				this.id++;
-				if (typeof this.count_buildings[building.type] == 'undefined') this.count_buildings[building.type] = 0;
-				this.count_buildings[building.type]++;
-				if (this.c.buildings.limits.types[building.type].max > 0 && this.count_buildings[building.type] > this.c.buildings.limits.types[building.type].max) fitted = false;
+				fitted = this._gen_check_limit("buildings", building);
 				if (fitted === true) {
-					//console.log("setup building", pos[0], pos[1], building);
-					for (var y = pos[1]; y < pos[1]+building.size[1]; y++) {
-						for (var x = pos[0]; x < pos[0]+building.size[0]; x++) {
-							this.m[y][x].id = this.id;
-							this.m[y][x].type = "building";
-							this.m[y][x].sub_type = building.type;
-							this.w[y][x] = 0;
-						}
-					}
+					this._gen_set_object("building", pos, building);
 				}
 			}
 		} else {
@@ -247,25 +340,96 @@ var city = function (c) {
 		return fitted;
 	}
 
-	this._gen_buildings_blocks_fit = function (b, building) {
-		//console.log("try to fit", building, "into", b);
+	this._gen_decorations_blocks_fit = function (b, obj) {
 		var fitted = false;
-		
-		// set into corners
-		this._gen_buildings_blocks_check_collision([b[0], b[1]], building, true);
-		this._gen_buildings_blocks_check_collision([b[0], b[3]-building.size[1]+1], building, true);
-		this._gen_buildings_blocks_check_collision([b[2]-building.size[0]+1, b[1]], building, true);
-		this._gen_buildings_blocks_check_collision([b[2]-building.size[0]+1, b[3]-building.size[1]+1], building, true);
-		
-		// set around the corners
-		for (var x = b[0]; x < b[2]-building.size[0]; x++) {
-			if (this._gen_buildings_blocks_check_collision([x, b[1]], building, true) || this._gen_buildings_blocks_check_collision([x, b[3]-building.size[1]+1], building, true)) {
+		var l = 100;	// limit random process to `l` iterations
+		var pos = [-1,-1];
+		while (l>0) {
+			rnd_x = Math.round(this.rnd(b[0], b[2]-obj.size[0]));
+			rnd_y = Math.round(this.rnd(b[1], b[3]-obj.size[1]));
+			pos = [rnd_x, rnd_y];
+			if ( 
+				this._gen_check_collission("decorations", pos, obj) && 
+				this._gen_check_distance(pos, obj) &&
+				this._gen_check_border("decorations", pos, obj, this.c.decorations.space_between_items, 'any') &&
+				this._gen_check_limit ("decorations", obj)
+			) {
+				this._gen_set_object("decorations", pos, obj);
 				fitted = true;
+				l=-1;
 			}
+			l--;
 		}
-		for (var y = b[1]; y < b[3]-building.size[1]; y++) {
-			if (this._gen_buildings_blocks_check_collision([b[0], y], building, true) || this._gen_buildings_blocks_check_collision([b[2]-building.size[0]+1, y], building, true)) {
-				fitted = true;
+		return fitted;		
+	}
+
+	this._gen_buildings_blocks_fit = function (b, building, step) {
+		var fitted = false;
+		var delta_road = Math.round(this.c.buildings.space_to_road);
+				
+		// set corners
+		if (step == 'corners') {
+			var tmp = this.shuffle(['topleft', 'topright', 'bottomleft', 'bottomright']);
+			for (var i = 0; i < 4; i++) {
+				if (Math.random()>0.5 && tmp[i] == 'topleft') this._gen_buildings_blocks_check_collision([b[0]+delta_road, b[1]+delta_road], building, true, 'topleft');
+				if (Math.random()>0.5 && tmp[i] == 'topright') this._gen_buildings_blocks_check_collision([b[0]+delta_road, b[3]-building.size[1]-delta_road+1], building, true, 'topright');
+				if (Math.random()>0.5 && tmp[i] == 'bottomleft') this._gen_buildings_blocks_check_collision([b[2]-delta_road-building.size[0]+1, b[1]+delta_road], building, true, 'bottomleft');
+				if (Math.random()>0.5 && tmp[i] == 'bottomright') this._gen_buildings_blocks_check_collision([b[2]-delta_road-building.size[0]+1, b[3]-building.size[1]-delta_road+1], building, true, 'bottomright');
+			}
+		} else {
+			
+			// set by contour
+			if (building.random_position) {
+				var random_process = true;
+				var rnd_x = 0;
+				var rnd_y = 0;
+				var l = 1000;	// limit random process to `l` iterations
+				if (building.position.indexOf('anywhere') > -1 && step == 'anywhere') {
+					while (l>0) {
+						rnd_x = Math.round(this.rnd(b[0]+delta_road, b[2]-building.size[0]-delta_road));
+						rnd_y = Math.round(this.rnd(b[1]+delta_road, b[3]-building.size[1]-delta_road));
+						if ( this._gen_buildings_blocks_check_collision([rnd_x, rnd_y], building, true, 'anywhere') ) {
+							fitted = true;
+						}
+						l--;
+					}
+				} else {
+					var result = false;
+					while (random_process && l>0) {
+						rnd_x = Math.round(this.rnd(b[0]+delta_road, b[2]-building.size[0]-delta_road));
+						rnd_y = Math.round(this.rnd(b[1]+delta_road, b[3]-building.size[1]-delta_road));
+						result = [
+								this._gen_buildings_blocks_check_collision([rnd_x, b[1]+delta_road], building, true, 'top'), 
+								this._gen_buildings_blocks_check_collision([rnd_x, b[3]-building.size[1]-delta_road+1], building, true, 'bottom'), 
+								this._gen_buildings_blocks_check_collision([b[0]+delta_road, rnd_y], building, true, 'left'),  
+								this._gen_buildings_blocks_check_collision([b[2]-building.size[0]-delta_road+1, rnd_y], building, true, 'right')
+							];
+						if ( result[0] || result[1] || result[2] || result[3] ) {
+							fitted = true;
+						} else {
+							random_process = false;
+						}
+						l--;
+					}
+				}
+				
+			} else {
+				for (var x = b[0]+delta_road; x < b[2]-building.size[0]-delta_road; x++) {
+					if (
+						this._gen_buildings_blocks_check_collision([x, b[1]+delta_road], building, true, 'top') || 
+						this._gen_buildings_blocks_check_collision([x, b[3]-building.size[1]-delta_road+1], building, true, 'bottom')
+					) {
+						fitted = true;
+					}
+				}
+				for (var y = b[1]+delta_road; y < b[3]-building.size[1]-delta_road; y++) {
+					if (
+						this._gen_buildings_blocks_check_collision([b[0]+delta_road, y], building, true, 'left') || 
+						this._gen_buildings_blocks_check_collision([b[2]-building.size[0]-delta_road+1, y], building, true, 'right')
+					) {
+						fitted = true;
+					}
+				}
 			}
 		}
 		
@@ -277,9 +441,68 @@ var city = function (c) {
 				this.gen_buildings_blocks(i);
 			}
 		} else {
+			this._gen_update_limits();
+			
+			// set corners at first
+			var b_limit = 0;
 			for (var b = 0; b < this.c.buildings.types.length; b++) {
-				while (this._gen_buildings_blocks_fit(this.blocks[i], this.c.buildings.types[b])) {
-					
+				b_limit = 100;
+				if (
+					this.c.buildings.types[b].position.indexOf('topleft') > -1 || 
+					this.c.buildings.types[b].position.indexOf('topright') > -1 || 
+					this.c.buildings.types[b].position.indexOf('bottomleft') > -1 || 
+					this.c.buildings.types[b].position.indexOf('bottomright') > -1
+				) {
+					while (b_limit > 0 && this._gen_buildings_blocks_fit(this.blocks[i], this.c.buildings.types[b], 'corners')) { b_limit--; }
+				}
+			}
+			
+			// set contour
+			for (var b = 0; b < this.c.buildings.types.length; b++) {
+				b_limit = 1000;
+				if (
+					this.c.buildings.types[b].position.indexOf('left') > -1 || 
+					this.c.buildings.types[b].position.indexOf('right') > -1 || 
+					this.c.buildings.types[b].position.indexOf('top') > -1 || 
+					this.c.buildings.types[b].position.indexOf('bottom') > -1 || 
+					this.c.buildings.types[b].position.indexOf('contour') > -1 
+				) {
+					while (b_limit > 0 && this._gen_buildings_blocks_fit(this.blocks[i], this.c.buildings.types[b], 'contour')) { 
+						b_limit--; 
+					}
+				}
+			}
+			
+			// special object (decorations) inside block
+			for (var b = 0; b < this.c.decorations.types.length; b++) {
+				b_limit = 1000;
+				if (Math.random() > 1-this.c.decorations.types[b].value) {
+					while (b_limit > 0 && this._gen_decorations_blocks_fit(this.blocks[i], this.c.decorations.types[b])) { 
+						b_limit--; 
+					}
+				}
+			}
+			
+			// set anywhere
+			for (var b = 0; b < this.c.buildings.types.length; b++) {
+				b_limit = 1000;
+				if (
+					this.c.buildings.types[b].position.indexOf('anywhere') > -1
+				) {
+					while (b_limit>0 && this._gen_buildings_blocks_fit(this.blocks[i], this.c.buildings.types[b], 'anywhere')) { b_limit--; }
+				}
+			}
+
+		}
+	}
+	
+	
+	this.gen_finish = function () {
+		for (var y=0;y<this.c.size[1];y++) {
+			for (var x=0;x<this.c.size[0];x++) {
+				if (this.m[y][x].type == 'none') {
+					this.m[y][x].type = 'ground';
+					this.w[y][x] = 1;
 				}
 			}
 		}
@@ -288,6 +511,16 @@ var city = function (c) {
 	// help functions
 	this.rnd = function (min, max) {
 		return parseInt((Math.random()*(max-min+1)+min));
+	}
+	
+	this.shuffle = function (arr) {
+		for (var i = arr.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		return arr;
 	}
 	
 	this.init(c);
--- city/tile_map.js
+++ city/tile_map.js
@@ -22,7 +22,11 @@ var tileMap = function (obj) {
 						'house_big': "#dd0000",
 						'house_big.rotated': "#dd0000",
 						'house_medium': "#aa0000",
+						'house_medium.rotated': "#aa0000",
+						'house_medium.corner': "#aa0000",
 						'house_small': "#770000",
+						'house_small.corner': "#770000",
+						'house_small.random': "#770000",
 					}
 			},
 			ground: {
@@ -36,27 +40,35 @@ var tileMap = function (obj) {
 			border: {
 				html: ".",
 				color: "black"
-			}
+			},
+			decorations: {
+				html: "*",
+				color: "blue"
+			},
+			unknown : {
+				html: "?",
+				color: "yellow"
+			},
 		},
 	
-	this.display = function (map) {
-		//console.log("map", map);
+	this.display = function (obj) {
+		var map = obj.map;
+		var walk = obj.walk;
 		var out = "";
 		for (var y = 0; y < map.length; y++) {
 			out +="<div style='white-space: nowrap;'>"
 			for (var x = 0; x < map[y].length; x++) {
-				var type = map[y][x].type;
+				var type = typeof map[y][x].type != 'undefined'?map[y][x].type:'unknown';
 				var sub_type = map[y][x].sub_type;
+				if (typeof this.tiles[type] == 'undefined') type = 'unknown';
 				var color = this.tiles[type].color;
-				if (typeof this.tiles[type] == 'undefined') type = 'none';
 				if (typeof sub_type == 'undefined') {
 					sub_type = '';
 				} else if (typeof this.tiles[type].types_color != 'undefined' && typeof this.tiles[type].types_color[sub_type] != 'undefined') {
 					color = this.tiles[type].types_color[sub_type];
 				}
-				//out+=this.tiles[type].html;
-				//out+='<div title="'+sub_type+'" style="background-color: '+color+'; border-top: 1px dotted blue; border-left: 1px dotted blue; display: inline-block; width:15px; height:15px; font-size: 10px;">'+(map[y][x].id==0?"&nbsp;":map[y][x].id)+'</div>';
-				out+='<div title="'+sub_type+'" style="background-color: '+color+'; display: inline-block; width:15px; height:15px; font-size: 10px;">'+(map[y][x].id==0?"&nbsp;":map[y][x].id)+'</div>';
+				//out+='<div title="'+sub_type+'" style="background-color: '+color+'; '+(walk[y][x]>0.5?'border-top: 1px dotted green; border-left: 1px dotted green; ':'margin-top:1px; margin-left:1px; ')+'display: inline-block; width:15px; height:15px; font-size: 10px;">'+(map[y][x].id==0?"&nbsp;":map[y][x].id)+'</div>';
+				out+='<div title="'+map[y][x].type+'.'+sub_type+'" style="background-color: '+color+'; display: inline-block; width:15px; height:15px; font-size: 10px;">'+(map[y][x].id==0?"&nbsp;":map[y][x].id)+'</div>';
 			}
 			out+="</div>";
 		}
